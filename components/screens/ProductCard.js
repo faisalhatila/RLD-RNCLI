c1a3343cbd5d0785eec87ff581e6ea60b1aad67c
faisalhatila <hatilas1994@gmail.com>
2020-12-23 22:34:50 +0500

    home screen done

--- components/screens/ProductCard.js
+++ components/screens/ProductCard.js
@@ -1,12 +1,112 @@
 import React from 'react';
-import {View} from 'react-native';
-import {LightText} from '../UI/Text';
-import DummyImage from '../assets/images/DummyImage.jpg';
+import {Image, ImageBackground, View} from 'react-native';
+import {BoldtText, LightText} from '../UI/Text';
+import DummyImage from '../../assets/images/DummyImage.jpg';
+import Star from '../../assets/images/Star.png';
+import BagGold from '../../assets/images/BagGold.png';
+import BagBlack from '../../assets/images/BagBlack.png';
+import BagWhite from '../../assets/images/BagWhite.png';
+import HeartWhite from '../../assets/images/HeartWhite.png';
+import HeartRed from '../../assets/images/HeartRed.png';
+import BigWhiteHeart from '../../assets/images/BigWhiteHeart.png';
+import BigRedHeart from '../../assets/images/BigRedHeart.png';
+import BigBagGold from '../../assets/images/BigBagGold.png';
 
-const ProductCard = () => {
+const ProductCard = (props) => {
   return (
-    <View>
-      <LightText>Card</LightText>
+    <View
+      style={{
+        width: '48%',
+        elevation: 1,
+        // borderWidth: 1,
+        borderRadius: 10,
+        overflow: 'hidden',
+        marginBottom: 10,
+      }}>
+      <View style={{}}>
+        <ImageBackground
+          source={DummyImage}
+          style={{width: '100%', height: 200}}
+          imageStyle={{resizeMode: 'cover'}}
+        />
+      </View>
+      <View
+        style={{
+          //   backgroundColor: '#4f4f4f',
+          backgroundColor: '#000',
+          paddingHorizontal: 5,
+          paddingVertical: 7,
+        }}>
+        <View style={{alignItems: 'center', flexDirection: 'row'}}>
+          <View style={{flex: 1}}>
+            <BoldtText style={{color: '#fff', fontFamily: 'serif'}}>
+              Women's Sexy Lingerie
+            </BoldtText>
+          </View>
+          <View>
+            <LightText style={{color: '#fff', fontFamily: 'serif'}}>
+              $15
+            </LightText>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              //   paddingHorizontal: 10,
+              alignItems: 'center',
+            }}>
+            <View>
+              <Image source={Star} />
+            </View>
+            <View style={{marginLeft: 5}}>
+              <LightText
+                style={{color: '#fff', fontFamily: 'serif', fontSize: 12}}>
+                4.3
+              </LightText>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            {/* <View> */}
+            <View
+              style={{
+                width: 20,
+                height: 20,
+                marginLeft: 5,
+                // backgroundColor: 'red',
+              }}>
+              <Image
+                source={BigBagGold}
+                style={{maxWidth: '100%', maxHeight: '100%'}}
+              />
+            </View>
+            {/* </View> */}
+            {/* <View style={{}}> */}
+            <View
+              style={{
+                width: 20,
+                height: 20,
+                marginLeft: 5,
+                // backgroundColor: 'red',
+              }}>
+              <Image
+                source={BigRedHeart}
+                style={{maxWidth: '100%', maxHeight: '100%'}}
+              />
+            </View>
+            {/* </View> */}
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Image, ImageBackground, View} from 'react-native';
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from 'react-native';
 import Input from '../components/UI/Input';
 import {BoldtText, LightText} from '../components/UI/Text';
 import SearchWhite from '../assets/images/SearchWhite.png';
@@ -9,8 +16,12 @@ import Bra from '../assets/images/Bra.png';
 import Lingeries from '../assets/images/Lingeries.png';
 import HomeCover1 from '../assets/images/HomeCover1.png';
 import HomeCover2 from '../assets/images/HomeCover2.png';
+import ProductCard from '../components/screens/ProductCard';
 
 const Home = () => {
+  const renderItem = ({item}) => {
+    return <ProductCard />;
+  };
   const categories = [
     {
       id: 1,
@@ -33,12 +44,43 @@ const Home = () => {
       image: Boxer,
     },
   ];
+  const products = [
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+    {
+      id: 3,
+    },
+    {
+      id: 4,
+    },
+    {
+      id: 5,
+    },
+    {
+      id: 6,
+    },
+    {
+      id: 7,
+    },
+    {
+      id: 8,
+    },
+    {
+      id: 9,
+    },
+    {
+      id: 10,
+    },
+  ];
   return (
-    <View style={{borderWidth: 1, flex: 1}}>
+    <View style={{flex: 1}}>
       <View
         style={{
           margin: 15,
-          borderWidth: 1,
           flex: 1,
         }}>
         <View
@@ -66,69 +108,100 @@ const Home = () => {
             <Image source={SearchWhite} />
           </View>
         </View>
-        <View
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 1,
-            marginVertical: 10,
-          }}>
-          <BoldtText
-            style={{fontSize: 22, fontFamily: 'serif', color: '#C99230'}}>
-            Categories
-          </BoldtText>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            borderWidth: 1,
-            marginVertical: 10,
-          }}>
-          {categories.map((category, i) => {
-            return (
-              <View
-                key={i}
-                style={{
-                  //   flex: 0.5,
-                  width: '22%',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  shadowColor: '#000',
-                  shadowOffset: {
-                    width: 0,
-                    height: 1,
-                  },
-                  shadowOpacity: 0.22,
-                  shadowRadius: 2.22,
-                  elevation: 1,
-                }}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginVertical: 10,
+            }}>
+            <BoldtText
+              style={{fontSize: 22, fontFamily: 'serif', color: '#C99230'}}>
+              Categories
+            </BoldtText>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginVertical: 10,
+            }}>
+            {categories.map((category, i) => {
+              return (
                 <View
+                  key={i}
                   style={{
-                    // backgroundColor: '#222',
-                    width: '100%',
-                    height: 50,
+                    elevation: 1,
+                    overflow: 'hidden',
+                    borderTopEndRadius: 5,
+                    borderTopLeftRadius: 5,
+                    borderBottomLeftRadius: 5,
+                    borderBottomEndRadius: 4.9,
+                    //   flex: 0.5,
+                    width: '22%',
                     justifyContent: 'center',
                     alignItems: 'center',
+                    //   shadowColor: '#000',
+                    //   shadowOffset: {
+                    //     width: 0,
+                    //     height: 1,
+                    //   },
+                    //   shadowOpacity: 0.22,
+                    //   shadowRadius: 2.22,
+                    //   borderWidth: 1,
                   }}>
-                  <Image source={category.image} />
+                  <View
+                    style={{
+                      // backgroundColor: '#222',
+                      width: '100%',
+                      height: 50,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Image source={category.image} />
+                  </View>
+                  <View style={{transform: [{translateY: -5}]}}>
+                    <LightText style={{fontSize: 11, fontFamily: 'serif'}}>
+                      {category.title}
+                    </LightText>
+                  </View>
                 </View>
-                <View>
-                  <LightText style={{fontSize: 11, fontFamily: 'serif'}}>
-                    {category.title}
-                  </LightText>
-                </View>
-              </View>
-            );
-          })}
-        </View>
-        <View style={{borderWidth: 1, marginVertical: 10}}>
-          <ImageBackground
-            source={HomeCover2}
-            style={{width: '100%', height: 70}}
-            imageStyle={{resizeMode: 'cover'}}
-          />
-        </View>
+              );
+            })}
+          </View>
+          <View style={{borderWidth: 1, marginVertical: 10}}>
+            <ImageBackground
+              source={HomeCover2}
+              style={{width: '100%', height: 150}}
+              imageStyle={{resizeMode: 'cover'}}
+            />
+          </View>
+          <View
+            style={{
+              // borderWidth: 1,
+              marginVertical: 10,
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+            }}>
+            {/* <SafeAreaView> */}
+            {/* <FlatList
+              data={products}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id}
+              numColumns={2}
+              columnWrapperStyle={{
+                justifyContent: 'space-between',
+                marginBottom: 10,
+              }}
+              //   extraData={selectedId}
+            /> */}
+            {/* </SafeAreaView> */}
+            {[...Array(10)].map((card, i) => {
+              return <ProductCard key={i} />;
+            })}
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
